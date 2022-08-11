bd96f273479c41c3e69811301796613ec2b8e569
emavi98 <emanuelvm04@gmail.com>
2022-08-11 12:09:27 +0200

    Backend 1.0 terminado

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
+require('dotenv').config();
+
+//console.log( process.env);
+
+/// Crear el servidor de express
+const app = express();
+
+// Base de datos
+dbConnection();
+
+//CORS
+app.use(cors())
+
+
+// Lectura y parseo del Body
+app.use( express.json() );
+
+//Rutas
+app.use('/api/auth', require('./routes/auth') );
+app.use('/api/events', require('./routes/events') );
+
+
+
+// Directorio Público
+app.use(express.static('public'));
+
+
+// Escuchar peticiones
+app.listen( process.env.PORT , () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+});
---
+++ routes/events.js
@@ -0,0 +1,33 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+//const { isDate } = require('../helpers/isDate')
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+
+const router = Router();
+
+// Validar con token JWT
+router.use( validarJWT);
+
+// Obtener eventos
+router.get('/', getEventos );
+
+// Crear nuevo evento
+router.post(
+            '/',
+            [
+                check('title','El titulo es obligatorio').not().isEmpty(),
+                check('start','Fecha inicio es obligatoria').isDate(),
+                check('end','Fecha Fin es obligatoria').isDate(),
+                validarCampos
+            ],
+             crearEvento );
+
+// Actualizar nuevo evento
+router.put('/:id', actualizarEvento );
+
+// Borrar evento
+router.delete('/:id', eliminarEvento );
+
+module.exports = router;
