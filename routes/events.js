9ad647eb331eba0af978518c6346c7237edeed3b
emavi98 <emanuelvm04@gmail.com>
2022-08-13 11:07:49 +0200

    CalendarApp Actualizada

--- routes/events.js
+++ routes/events.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-//const { isDate } = require('../helpers/isDate')
+const { isDate } = require('../helpers/isDate')
 const { validarCampos } = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
@@ -18,8 +18,8 @@ router.post(
             '/',
             [
                 check('title','El titulo es obligatorio').not().isEmpty(),
-                check('start','Fecha inicio es obligatoria').isDate(),
-                check('end','Fecha Fin es obligatoria').isDate(),
+                check('start','Fecha inicio es obligatoria').custom( isDate ) ,
+               check('end','Fecha Fin es obligatoria').custom( isDate ),
                 validarCampos
             ],
              crearEvento );
